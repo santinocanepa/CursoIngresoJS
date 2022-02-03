ce6d2855dd1020ac0181440917cf2ca042af119c
santino canepa <canepasanti@gmail.com>
2022-02-03 16:29:57 -0300

    ejercicio 2 hecho

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,29 @@
 /*
+Santino Canepa
+ejercicio 2
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+/* para declarar variables se usa la palabra reservada let */ 
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	nombre = prompt("ingrese su nombre");
+
+	alert(nombre);
+
+	let apellido;
+
+	apellido = prompt("ingrese su apellido");
+
+	alert(apellido);
+
+	alert("muchas gracias");
 }
 
+/* con let hago una variable 
+usando la variable + el prompt hago que salga la caja para que el 
+usario logre poner su dato, y finalmente el alert tira un 
+mensaje sin "" para que se refiera a la variable NOMBRE, osea lo que ponga
+el mismo usuario 
+*/
