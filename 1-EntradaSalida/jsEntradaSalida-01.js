852736c6e9d272b9ad545bf63272238602231396
santino canepa <canepasanti@gmail.com>
2022-02-03 15:10:44 -0300

    ultima actualizacion ej 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,12 @@
 /*
+Santino Canepa / ejercicio 1
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundito");
+	alert("Hola");
+	alert("me");
+	alert("llamo");
+	alert("santino");
+	alert(":)");
 }
-
